fecd5ac1285ae590effc65c1ac8aa7008593aac0
Ali Sürmeli <alisrml20@gmail.com>
2025-01-13 03:01:07 +0300

    Time: 0 ms (100%), Space: 42.9 MB (21.69%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        if(nums.length == 0||nums.length == 1){return nums;}
+        for(int i = 1;i<nums.length;i++){
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}
